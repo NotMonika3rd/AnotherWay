a05f5a66871e263158737f6e0b60646563e6e198
NotMonika3rd <l73583@126.com>
2026-02-18 22:20:18 +0800

    init

---
+++ groovy/postInit/main.groovy
@@ -0,0 +1,4 @@
+mods.calculator.basic_calculator.recipeBuilder()
+    .input(item('deepresonance:machine_frame'),item('minecraft:diamond_sword'))
+    .output(item('cyberware:surgery'))
+    .register()
